e54e6eca4cc384b33cd1d4515516b82b7f17f000
Josh Grancell <josh@joshgrancell.com>
2015-11-03 06:34:18 -0500

    Corrected SQL column name on cron disabler

--- classes/Character.class.php
+++ classes/Character.class.php
@@ -693,7 +693,7 @@ class Character {
 			$stmt->execute(array($this->keyID));
 
 			// Disabling the key.
-			$stmt = $this->db->prepare('UPDATE core_cron SET keystatus = 0 WHERE userid = ?');
+			$stmt = $this->db->prepare('UPDATE core_cron SET keystatus = 0 WHERE api_keyID = ?');
 			$stmt->execute(array($this->keyID));
 
 			// Adding the error output to our error_logging DB table
